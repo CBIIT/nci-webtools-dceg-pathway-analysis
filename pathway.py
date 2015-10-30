470ca09b3dc93313e7c6e537df7fffaf0f8e350b
Tony Hall <tony.hall@nih.gov>
2015-10-30 13:09:08 -0400

    routing calls to common folder, changing to min/max width queries

--- pathway.py
+++ pathway.py
@@ -2,7 +2,7 @@ import json
 import os
 import sys
 import time
-from flask import Flask, Response, request,jsonify
+from flask import Flask, Response, request,jsonify, send_from_directory
 
 app = Flask(__name__, static_folder='static', static_url_path="")
 
@@ -11,6 +11,10 @@ class Pathway:
     @app.route('/')
     def root():
         return app.send_static_file('index.html')
+
+    @app.route('/common/<path:filename>')
+    def custom_static(filename):
+        return send_from_directory(app.config['COMMON_PATH'], filename)
 
     @app.route('/options/pathway_options', methods=['GET'])
     @app.route('/options/pathway_options/', methods=['GET'])
@@ -105,6 +109,7 @@ class Pathway:
             argFunctions.get(lastValue, self.doNothing)(value)
             lastValue = value
         argFunctions.get(lastValue, self.doNothing)(None)
+        app.config['COMMON_PATH'] = '../common/'
         app.config['UPLOAD_FOLDER'] = 'uploads'
         app.config["ALLLOWED_TYPES"] = ['study', 'pathway']
         app.run(host='0.0.0.0', port=self.WEB_PORT, debug=self.DEBUG)
