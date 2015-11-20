bcfd6ab52b4324d20c6c5ec5220b6deb7d94c7d4
Wesley Obenshain <wesley.obenshain@nci.nih.gov>
2015-11-19 20:58:46 -0500

    validation was requiring a selected pathway when a file was chosen

--- src/app/scripts/validators.js
+++ src/app/scripts/validators.js
@@ -8,7 +8,7 @@ $(function(){
         database_pathway: {
             required: {
                 depends:function(element) {
-                    return $("#database_pathway_option").is(":checked") || element.value.length === 0;
+                    return $("#database_pathway_option").is(":checked");
                 }
             }
         },
