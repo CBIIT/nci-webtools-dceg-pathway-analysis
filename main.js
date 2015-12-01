1e2e6bd47611d7e859077b7be37ba3404aa8dae0
Wesley Obenshain <wesley.obenshain@nci.nih.gov>
2015-12-01 14:07:12 -0500

    Attempting rewrite to be served by apache

--- main.js
+++ main.js
@@ -297,164 +297,163 @@ function displayErrors(el, messagesArray){
 }
 
 
-var serviceBase = "pathwayRest/";
 var buttons = $("button").button();
 
 $(function(){
-    var count = 2;
-    var hold = function() {
-        count--;
-        if (count <= 0)
-            post_request();
-    };
-   
-    retrieve_pathways().then(apply_options_combobox($(pathForm.database_pathway)), get_options_error("pathway")).always(hold);
-    retrieve_populations().then(apply_multiselect_options($(pathForm.population)), get_options_error("population")).always(hold);
+  var count = 2;
+  var hold = function() {
+    count--;
+    if (count <= 0)
+      post_request();
+  };
+ 
+  retrieve_pathways().then(apply_options_combobox($(pathForm.database_pathway)), get_options_error("pathway")).always(hold);
+  retrieve_populations().then(apply_multiselect_options($(pathForm.population)), get_options_error("population")).always(hold);
 });
 
 function pre_request() {
-   
-    $("#spinner").show();
+ 
+  $("#spinner").show();
 
-   
-    $(pathForm).find(":input").prop("disabled",true);
-    buttons.button("disable");
+ 
+  $(pathForm).find(":input").prop("disabled",true);
+  buttons.button("disable");
 }
 
 function post_request() {
-   
-    $("button#calculate").show();
-    $("progress, #spinner").hide();
+ 
+  $("button#calculate").show();
+  $("progress, #spinner").hide();
 
-   
-    buttons.button("enable");
-    $(pathForm).find(":input").removeAttr("disabled");
+ 
+  buttons.button("enable");
+  $(pathForm).find(":input").removeAttr("disabled");
 }
 
 function submission_result(response) {
-    if(response.success){
-        resetForm();
+  if(response.success){
+    resetForm();
 
-       
-        $( "#successBox #message" ).text(response.message);
-        $( "#successBox").show();
-        document.querySelector("#successBox").scrollIntoView(true);
+   
+    $( "#successBox #message" ).text(response.message);
+    $( "#successBox").show();
+    document.querySelector("#successBox").scrollIntoView(true);
 
-        setTimeout(function(){
-            $( "#successBox" ).fadeOut().hide();
-            $( "#successBox #message" ).html("");
-        }, 10000);
-    }
+    setTimeout(function(){
+      $( "#successBox" ).fadeOut().hide();
+      $( "#successBox #message" ).html("");
+    }, 10000);
+  }
 }
 
 function apply_multiselect_options(element){
-    population_labels ={'AFR':{'fullName':'African','subPopulations':{'YRI':'Yoruba in Ibadan, Nigera','LWK':'Luhya in Webuye, Kenya','GWD':'Gambian in Western Gambia','MSL':'Mende in Sierra Leone','ESN':'Esan in Nigera','ASW':'Americans of African Ancestry in SW USA','ACB':'African Carribbeans in Barbados'}},'AMR':{'fullName':'Ad Mixed American','subPopulations':{'MXL':'Mexican Ancestry from Los Angeles, USA','PUR':'Puerto Ricans from Puerto Rico','CLM':'Colombians from Medellin, Colombia','PEL':'Peruvians from Lima, Peru'}},'EAS':{'fullName':'East Asian','subPopulations':{'CHB':'Han Chinese in Bejing, China','JPT':'Japanese in Tokyo, Japan','CHS':'Southern Han Chinese','CDX':'Chinese Dai in Xishuangbanna, China','KHV':'Kinh in Ho Chi Minh City, Vietnam'}},'EUR':{'fullName':'European','subPopulations':{'CEU':'Utah Residents from North and West Europe','TSI':'Toscani in Italia','FIN':'Finnish in Finland','GBR':'British in England and Scotland','IBS':'Iberian population in Spain'}},'SAS':{'fullName':'South Asian','subPopulations':{'GIH':'Gujarati Indian from Houston, Texas','PJL':'Punjabi from Lahore, Pakistan','BEB':'Bengali from Bangladesh','STU':'Sri Lankan Tamil from the UK','ITU':'Indian Telugu from the UK'}}};
+  population_labels ={'AFR':{'fullName':'African','subPopulations':{'YRI':'Yoruba in Ibadan, Nigera','LWK':'Luhya in Webuye, Kenya','GWD':'Gambian in Western Gambia','MSL':'Mende in Sierra Leone','ESN':'Esan in Nigera','ASW':'Americans of African Ancestry in SW USA','ACB':'African Carribbeans in Barbados'}},'AMR':{'fullName':'Ad Mixed American','subPopulations':{'MXL':'Mexican Ancestry from Los Angeles, USA','PUR':'Puerto Ricans from Puerto Rico','CLM':'Colombians from Medellin, Colombia','PEL':'Peruvians from Lima, Peru'}},'EAS':{'fullName':'East Asian','subPopulations':{'CHB':'Han Chinese in Bejing, China','JPT':'Japanese in Tokyo, Japan','CHS':'Southern Han Chinese','CDX':'Chinese Dai in Xishuangbanna, China','KHV':'Kinh in Ho Chi Minh City, Vietnam'}},'EUR':{'fullName':'European','subPopulations':{'CEU':'Utah Residents from North and West Europe','TSI':'Toscani in Italia','FIN':'Finnish in Finland','GBR':'British in England and Scotland','IBS':'Iberian population in Spain'}},'SAS':{'fullName':'South Asian','subPopulations':{'GIH':'Gujarati Indian from Houston, Texas','PJL':'Punjabi from Lahore, Pakistan','BEB':'Bengali from Bangladesh','STU':'Sri Lankan Tamil from the UK','ITU':'Indian Telugu from the UK'}}};
 
-    return function(data) {
-        $.each(data, function (key, population) {
-            var optGroup = $(element).has("optgroup[label='" + population.group + "']").length > 0 ? $(element).find("optgroup[label='" + population.group + "']") : $("<optgroup label='" + population.group + "'/>");
+  return function(data) {
+    $.each(data, function (key, population) {
+      var optGroup = $(element).has("optgroup[label='" + population.group + "']").length > 0 ? $(element).find("optgroup[label='" + population.group + "']") : $("<optgroup label='" + population.group + "'/>");
 
-            var superLabel = population_labels[population.group].fullName;
-            var subLabel = population_labels[population.group].subPopulations[population.subPopulation];
-            var option = $("<option />", { value: population.group+"|"+population.subPopulation, text: subLabel });
+      var superLabel = population_labels[population.group].fullName;
+      var subLabel = population_labels[population.group].subPopulations[population.subPopulation];
+      var option = $("<option />", { value: population.group+"|"+population.subPopulation, text: subLabel });
 
-            optGroup.append(option);
+      optGroup.append(option);
 
-            $(element).append(optGroup).multipleSelect('refresh');
-        });
+      $(element).append(optGroup).multipleSelect('refresh');
+    });
 
-        $(element).multipleSelect("uncheckAll");
+    $(element).multipleSelect("uncheckAll");
 
-        $(".group input[type='checkbox']").on("click", function(e) {
-            var targetedElement = e.target;
-           
-            var otherGroups = $("label.optgroup").not(this.parentElement);
+    $(".group input[type='checkbox']").on("click", function(e) {
+      var targetedElement = e.target;
+     
+      var otherGroups = $("label.optgroup").not(this.parentElement);
 
-            otherGroups.find("input[type='checkbox']").not(targetedElement).each(function(i, el){
-                if(el != targetedElement && el.checked) {
-                    $(el).trigger("click");
-                }
-            });
+      otherGroups.find("input[type='checkbox']").not(targetedElement).each(function(i, el){
+        if(el != targetedElement && el.checked) {
+          $(el).trigger("click");
+        }
+      });
 
-            if(!targetedElement.checked){
-                $(targetedElement).trigger("click");
-            }
-           
-        });
-    };
+      if(!targetedElement.checked){
+        $(targetedElement).trigger("click");
+      }
+     
+    });
+  };
 }
 
 function apply_options_combobox(element){
-    return function(data) {
-        data.forEach(function(item, i) {
-            var option = $("<option></option>");
-            $(option).val(item.code);
-            $(option).text(item.text);
-            element.append(option);
-        });
+  return function(data) {
+    data.forEach(function(item, i) {
+      var option = $("<option></option>");
+      $(option).val(item.code);
+      $(option).text(item.text);
+      element.append(option);
+    });
 
-        element.combobox();
-    };
+    element.combobox();
+  };
 }
 
 function submission_error(request, statusText, error) {
-    var errorObj = JSON.parse(request.responseText);
+  var errorObj = JSON.parse(request.responseText);
 
-    displayErrors("#errorDisplay",
-                  ["The request failed with the following message: <br/> "+ errorObj.message]);
+  displayErrors("#errorDisplay",
+          ["The request failed with the following message: <br/> "+ errorObj.message]);
 }
 
 function get_options_error(option_type) {
-    return function(request, statusText, error) {
-        displayErrors("#errorDisplay",
-                      ["There was a problem retrieving the " + option_type + " options from the server. Try again later."]);
-    };
+  return function(request, statusText, error) {
+    displayErrors("#errorDisplay",
+          ["There was a problem retrieving the " + option_type + " options from the server. Try again later."]);
+  };
 }
 
 function sendForm(formData) {
-    return $.ajax({
-        beforeSend: pre_request,
-        type: pathForm.method,
-        url: pathForm.action,
-        data: formData,
-        cache: false,
-        processData: false,
-        contentType: false,
-        xhr: function() {
-            var myXhr = $.ajaxSettings.xhr();
-            if (myXhr.upload) {
-                myXhr.upload.addEventListener("progress", function(other) {
-                    if (other.lengthComputable) {
-                        $("progress").attr({value:other.loaded,max:other.total});
-                    }
-                }, false);
-            }
-            return myXhr;
-        },
-        dataType: "json"
-    });
+  return $.ajax({
+    beforeSend: pre_request,
+    type: pathForm.method,
+    url: pathForm.action,
+    data: formData,
+    cache: false,
+    processData: false,
+    contentType: false,
+    xhr: function() {
+      var myXhr = $.ajaxSettings.xhr();
+      if (myXhr.upload) {
+        myXhr.upload.addEventListener("progress", function(other) {
+          if (other.lengthComputable) {
+            $("progress").attr({value:other.loaded,max:other.total});
+          }
+        }, false);
+      }
+      return myXhr;
+    },
+    dataType: "json"
+  });
 }
 
 function retrieve_pathways(){
-    return $.ajax({
-        url: serviceBase + "options/pathway_options/",
-        type: "GET",
-        beforeSend: pre_request,
-        contentType: "application/json",
-        dataType: "json",
-        cache: false
-    });
+  return $.ajax({
+    url: "pathway_options.json",
+    type: "GET",
+    beforeSend: pre_request,
+    contentType: "application/json",
+    dataType: "json",
+    cache: false
+  });
 }
 
 function retrieve_populations(){
-    return $.ajax({
-        url: serviceBase + "options/population_options/",
-        type: "GET",
-        beforeSend: pre_request,
-        contentType: "application/json",
-        dataType: "json",
-        cache: false
-    });
+  return $.ajax({
+    url: "population_options.json",
+    type: "GET",
+    beforeSend: pre_request,
+    contentType: "application/json",
+    dataType: "json",
+    cache: false
+  });
 }
 
 
