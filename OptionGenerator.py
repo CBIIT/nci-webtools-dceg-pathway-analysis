855c5e43dcfd69f34e0b19d09db2e906d93a1371
Wesley Obenshain <wesley.obenshain@nci.nih.gov>
2015-12-08 12:19:36 -0500

    NCIATWP-244 Python on Linux doesn't read the filesystem in Alphabetical Order. Weird.

--- OptionGenerator.py
+++ OptionGenerator.py
@@ -9,7 +9,7 @@ class OptionGenerator:
 
   def pathway_options(self):
     options = []
-    for pathways_file in os.listdir(self.config[OptionGenerator.PATHWAY_FOLDER]):
+    for pathways_file in sorted(os.listdir(self.config[OptionGenerator.PATHWAY_FOLDER])):
       if pathways_file.endswith(".txt") or pathways_file.endswith(".pathway") or pathways_file.endswith(".txt.xls.gz"):
         pathways_filename = pathways_file.split(".")[0]
         options.append({
@@ -20,10 +20,9 @@ class OptionGenerator:
 
   def population_options(self):
     options = []
-    for super_population in [name for name in os.listdir(self.config[OptionGenerator.POPULATION_FOLDER])
-      if os.path.isdir(os.path.join(self.config[OptionGenerator.POPULATION_FOLDER], name))]:
+    for super_population in [name for name in sorted(os.listdir(self.config[OptionGenerator.POPULATION_FOLDER])) if os.path.isdir(os.path.join(self.config[OptionGenerator.POPULATION_FOLDER], name))]:
         subpopulation_path = os.path.join(self.config[OptionGenerator.POPULATION_FOLDER], super_population)
-        for subpopulation in [sub_name for sub_name in os.listdir(subpopulation_path) if os.path.isdir(subpopulation_path)]:
+        for subpopulation in [sub_name for sub_name in sorted(os.listdir(subpopulation_path)) if os.path.isfile(os.path.join(subpopulation_path, sub_name))]:
           population_code = subpopulation.split(".")[0]
           options.append({
                            'group': super_population,
